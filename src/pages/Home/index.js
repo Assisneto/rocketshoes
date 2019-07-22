981a3e7035df0b897c61b581d487bf6540adbb2b
Luciano Tavernard <lucianotavernard@gmail.com>
2019-07-22 20:57:36 -0300

    Adicionado a exibição da quantidade do item adicionado ao carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -36,6 +36,7 @@ class Home extends Component {
 
   render() {
     const { products } = this.state;
+    const { amount } = this.props;
 
     return (
       <ProductList>
@@ -48,6 +49,7 @@ class Home extends Component {
             <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
                 <MdAddShoppingCart size={16} color="#fff" />
+                {amount[product.id] || 0}
               </div>
 
               <span>Adicionar ao carrinho</span>
@@ -59,9 +61,13 @@ class Home extends Component {
   }
 }
 
+const mapStateToProps = state => ({
+  amount: state.cart.reduce((amount, product) => ({ ...amount, [product.id]: product.amount }), {}),
+});
+
 const mapDispatchToProps = dispatch => bindActionCreators(CartActions, dispatch);
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(Home);
